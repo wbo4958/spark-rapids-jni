a425f6ea24245d3900784e3b85fb4a4eb1f3befc
Bobby Wang <wbo4958@gmail.com>
2022-08-11 16:43:46 +0800

    update

--- src/test/java/com/nvidia/spark/rapids/jni/CudaIPCTest.java
+++ src/test/java/com/nvidia/spark/rapids/jni/CudaIPCTest.java
@@ -1,5 +1,6 @@
 package com.nvidia.spark.rapids.jni;
 
+import java.beans.Transient;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
@@ -9,14 +10,23 @@ import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.FileChannel;
+import java.util.Arrays;
 import java.util.Base64;
 
 import org.junit.jupiter.api.Test;
 
 import ai.rapids.cudf.BufferType;
 import ai.rapids.cudf.ColumnVector;
+import ai.rapids.cudf.ContiguousTable;
+import ai.rapids.cudf.DeviceMemoryBuffer;
+import ai.rapids.cudf.DeviceMemoryBufferView;
 import ai.rapids.cudf.HostColumnVector;
 import ai.rapids.cudf.Table;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class CudaIPCTest {
@@ -38,20 +48,116 @@ public class CudaIPCTest {
     }
   }
 
+  // working
+  @Test
+  void testFilter() throws IOException, InterruptedException {
+    ColumnVector cv = ColumnVector.fromInts(101, 32, 3, 4, 5);
+    Table t1 = new Table(cv);
+    Boolean[] maskVals = new Boolean[5];
+    Arrays.fill(maskVals, true);
+    ColumnVector mask = ColumnVector.fromBoxedBooleans(maskVals);
+    Table result = t1.filter(mask);
+    writeToFile(result.getColumn(0), "/tmp/cuda.ipc");
+  }
+
+  @Test
+  void testConcatenate() throws IOException, InterruptedException {
+    ColumnVector cv = ColumnVector.fromInts(101, 32);
+    ColumnVector cv1 = ColumnVector.fromInts(81, 6);
+    Table table = new Table(cv, cv1);
+    ColumnVector cv2 = ColumnVector.fromInts(13, 4, 589);
+    ColumnVector cv3 = ColumnVector.fromInts(12, 13, 14);
+    Table table1 = new Table(cv2, cv3);
+    Table result = Table.concatenate(table.contiguousSplit()[0].getTable(), table1.contiguousSplit()[0].getTable());
+    writeToFile(result.getColumn(0), "/tmp/cuda.ipc");
+    writeToFile(result.getColumn(1), "/tmp/cuda.ipc1");
+    System.out.println("------------------");
+    Thread.sleep(1000000000);
+  }
+
+
+  // not-working
+  @Test
+  void testContinuseTable() throws IOException, InterruptedException {
+    ColumnVector cv = ColumnVector.fromInts(101, 32, 3, 4, 5);
+    ColumnVector cv1 = ColumnVector.fromInts(81, 6, 12, 13, 14);
+    Table table = new Table(cv, cv1);
+    ContiguousTable split[] = table.contiguousSplit();
+    byte[] bytes = CudaIPC.getCudaIpcMemHandle(split[0].getBuffer().getAddress());
+    ByteBuffer metaBuffer = split[0].getMetadataDirectBuffer().order(ByteOrder.nativeOrder());
+    PackedColumns x = PackedColumns.parseSerializedColumns(metaBuffer);
+
+//    writeToFile(total, "/tmp/cuda.ipc");
+
+    System.out.println("------------------");
+//    Thread.sleep(1000000000);
+  }
+
+  void writeToFile(ByteBuffer bb, String name) throws IOException {
+    File file = new File(name);
+    if (file.exists()) {
+      file.delete();
+    }
+
+    FileOutputStream fileOutputStream = new FileOutputStream(file);
+    FileChannel fc = fileOutputStream.getChannel();
+    fc.write(bb);
+    fileOutputStream.flush();
+    fileOutputStream.close();
+  }
+
+  void writeToFile(ColumnVector cv, String name) throws IOException {
+    long ptr = cv.getDeviceBufferFor(BufferType.DATA).getAddress();
+    byte[] handle = CudaIPC.getCudaIpcMemHandle(ptr);
+
+    CudaIPC.checkGpuMemory(ptr, (int)cv.getRowCount());
+    File file = new File(name);
+    if (file.exists()) {
+      file.delete();
+    }
+
+    FileOutputStream fileOutputStream = new FileOutputStream(file);
+    fileOutputStream.write(handle);
+    fileOutputStream.close();
+  }
+
   @Test
   void test() throws InterruptedException, IOException {
 
-    Table table = Table.readParquet(new File("/home/bobwang/tmp/cuda_ipc/person.parquet"));
     ColumnVector cv;
-    cv = table.getColumn(0);
-    HostColumnVector hcv = cv.copyToHost();
-    for (int i = 0; i < hcv.getRowCount(); i++) {
-      System.out.print(hcv.getInt(i) + " ");
+    Table table;
+//    Table table = Table.readParquet(new File("/home/bobwang/tmp/cuda_ipc/person.parquet"));
+//    cv = table.getColumn(0);
+//    HostColumnVector hcv = cv.copyToHost();
+//    for (int i = 0; i < hcv.getRowCount(); i++) {
+//      System.out.print(hcv.getInt(i) + " ");
+//    }
+//    CudaIPC.checkGpuMemory(cv.getDeviceBufferFor(BufferType.DATA).getAddress(), (int)cv.getRowCount());
+//    System.out.println();
+    ColumnVector cv2 = ColumnVector.fromInts(7, 8);
+    cv = ColumnVector.fromInts(11, 32, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
+    Table t1 = new Table(cv);
+//    ContiguousTable x[] = t1.contiguousSplit(2, 5, 9);
+//    table = Table.concatenate(x[0].getTable(), x[1].getTable());
+//    table = x[1].getTable();
+    ColumnVector[] xx = cv.slice(2, 5);
+//    cv = table.getColumn(0);
+    cv = xx[0];
+    DeviceMemoryBufferView dmb = (DeviceMemoryBufferView) cv.getDeviceBufferFor(BufferType.DATA);
+    int offset = dmb.getOffset();
+    System.out.println("offset " + offset);
+
+//    cv = ColumnVector.fromInts(11, 32, 3, 4, 5, 6, 9, 10, 20, 38);
+    long ptr = cv.getDeviceBufferFor(BufferType.DATA).getAddress();
+    byte[] handle1 = CudaIPC.getCudaIpcMemHandle(ptr);
+    for (int i = 0; i < handle1.length; i++) {
+      System.out.print(handle1[i] + " ");
     }
     System.out.println();
-//    cv = ColumnVector.fromInts(11, 32, 3, 4, 5, 6);
-    System.out.println("ptr is: " + String.format("0x%08x", cv.getDeviceBufferFor(BufferType.DATA).getAddress()));
-    byte[] handle = CudaIPC.getCudaIpcMemHandle(cv.getDeviceBufferFor(BufferType.DATA).getAddress());
+//    ptr = ptr + 4;
+    System.out.println("ptr is: " + String.format("0x%08x", ptr));
+    CudaIPC.checkGpuMemory(ptr, 5);
+    byte[] handle = CudaIPC.getCudaIpcMemHandle(ptr);
     for (int i = 0; i < handle.length; i++) {
       System.out.print(handle[i] + " ");
     }
@@ -73,7 +179,7 @@ public class CudaIPCTest {
 
     FileOutputStream fileOutputStream1 = new FileOutputStream(file1);
     fileOutputStream1.write("hello".getBytes());
-    long ptr = cv.getDeviceBufferFor(BufferType.DATA).getAddress();
+    ptr = cv.getDeviceBufferFor(BufferType.DATA).getAddress();
 
     FileWriter fileWriter = new FileWriter(file1);
     String cudaIpcInfo = Base64.getEncoder().encodeToString(handle);
@@ -82,5 +188,33 @@ public class CudaIPCTest {
     fileWriter.close();
     System.out.println("ptr is " + ptr);
     Thread.sleep(1000000000);
+  }
+
+  @Test
+  void testContiguousSplit() {
+    ContiguousTable[] splits = null;
+    try (Table t1 = new Table.TestBuilder()
+        .column(10, 12, 14, 16, 18, 20, 22, 24, null, 28)
+        .column(50, 52, 54, 56, 58, 60, 62, 64, 66, null)
+        .decimal32Column(-3, 10, 12, 14, 16, 18, 20, 22, 24, null, 28)
+        .decimal64Column(-8, 50L, 52L, 54L, 56L, 58L, 60L, 62L, 64L, 66L, null)
+        .build()) {
+      splits = t1.contiguousSplit(2, 5, 9);
+      assertEquals(4, splits.length);
+      assertEquals(2, splits[0].getRowCount());
+      assertEquals(2, splits[0].getTable().getRowCount());
+      assertEquals(3, splits[1].getRowCount());
+      assertEquals(3, splits[1].getTable().getRowCount());
+      assertEquals(4, splits[2].getRowCount());
+      assertEquals(4, splits[2].getTable().getRowCount());
+      assertEquals(1, splits[3].getRowCount());
+      assertEquals(1, splits[3].getTable().getRowCount());
+    } finally {
+      if (splits != null) {
+        for (int i = 0; i < splits.length; i++) {
+          splits[i].close();
+        }
+      }
+    }
   }
 }
